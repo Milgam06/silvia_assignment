5cd40cf40638603ef2f74c304346bad321ac8d75
Milgam06 <milgamfruit@gmail.com>
2024-11-09 19:33:57 +0900

    Initial Commit

---
+++ src/components/style/TextStyle.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const TextStyle = StyleSheet.create({
+  default: {
+    fontSize: 12,
+    fontWeight: 300,
+    color: "#000",
+  },
+});
---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+import { TextStyle } from "@/components";
+
+export const Home: React.FC = () => {
+  return (
+    <View>
+      <Text style={TextStyle.default}>
+        Open up App.tsx to start on your app!
+      </Text>
+    </View>
+  );
+};
