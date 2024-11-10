58e9ccb64631d1b4d66ddf0a8fc0c39c232dec59
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-11-11 00:05:31 +0900

    feat: add AnswerBox

--- src/screens/dsst/Test.tsx
+++ src/screens/dsst/Test.tsx
@@ -1,5 +1,4 @@
-import { useRef, useEffect } from "react";
-import { View, Text, Animated } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 import { TextStyle, QuestionBox } from "@/components";
@@ -10,9 +9,9 @@ import { TestViewStyle } from "./styled";
 export const Test: React.FC = () => {
   return (
     <>
-      <View style={TestViewStyle.keyView}>
+      <View style={TestViewStyle.keyAndImageContainer}>
         {KEY_IMAGE.map((item, i) => (
-          <View style={TestViewStyle.keyAndImageView}>
+          <View key={i} style={TestViewStyle.keyAndImageView}>
             <View style={{ borderBottomWidth: 1 }}>
               <Text style={TextStyle.testKey}>{i + 1}</Text>
             </View>
@@ -20,12 +19,29 @@ export const Test: React.FC = () => {
           </View>
         ))}
       </View>
-      <View style={TestViewStyle.questionKeyView}>
-        <QuestionBox questionNumber={1} answering="A" />
+      <View style={TestViewStyle.questionKeyContainer}>
+        <QuestionBox questionNumber={1} answering={KEY_IMAGE[0].image} />
       </View>
-      <View style={TestViewStyle.answerContainer}>
-        <Text style={TextStyle.default}>This is a test page</Text>
-      </View>
+
+      <FlatList
+        style={TestViewStyle.answerContainer}
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+          flexGrow: 1,
+        }}
+        scrollEnabled={false}
+        data={KEY_IMAGE}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <View style={TestViewStyle.answerView}>
+              <FontAwesomeIcon icon={item.image} size={30} />
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.toString()}
+        numColumns={3}
+      />
     </>
   );
 };
--- src/screens/dsst/styled.ts
+++ src/screens/dsst/styled.ts
@@ -1,8 +1,8 @@
 import { StyleSheet } from "react-native";
 
 export const TestViewStyle = StyleSheet.create({
-  keyView: {
-    flex: 1,
+  keyAndImageContainer: {
+    flex: 0.6,
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
@@ -15,21 +15,27 @@ export const TestViewStyle = StyleSheet.create({
     gap: 10,
     padding: 10,
     backgroundColor: "#fff",
-    borderWidth: 0.8,
+    borderWidth: 0.6,
   },
-  questionKeyView: {
+
+  questionKeyContainer: {
     flex: 0.6,
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "row",
-    gap: 10,
-    overflow: "hidden",
     backgroundColor: "#f5ebe0",
   },
   answerContainer: {
     flex: 1,
+    backgroundColor: "#f5ebe0",
+  },
+  answerView: {
+    width: 100,
+    height: 100,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f5ebe0",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    margin: 4,
   },
 });
