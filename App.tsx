8ed689f834000687d1d9e3b1d5afab9a4b91e6ca
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-11-10 14:33:13 +0900

    feat: add Navigation to route screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { Home } from "@/screens";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Home, Test } from "@/screens";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <Home />
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Test" component={Test} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
