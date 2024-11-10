fb1441c4f5c56564466c3e64a114811579170db3
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-11-11 01:05:58 +0900

    feat: add questionBoxKey test style

--- src/components/style/TextStyle.ts
+++ src/components/style/TextStyle.ts
@@ -3,13 +3,19 @@ import { StyleSheet } from "react-native";
 export const TextStyle = StyleSheet.create({
   default: {
     fontSize: 12,
-    fontWeight: 300,
+    fontWeight: "300",
     color: "#000",
   },
 
-  test: {
-    fontSize: 48,
-    fontWeight: "semi-bold",
+  questionBoxKey: {
+    fontSize: 72,
+    fontWeight: "bold",
+    color: "#000",
+  },
+
+  testKey: {
+    fontSize: 36,
+    fontWeight: "300",
     color: "#000",
   },
 });
