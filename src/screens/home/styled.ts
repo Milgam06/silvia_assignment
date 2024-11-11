16c865857699c211587ec4b9c595b9849287c37f
Milgam06 <milgamfruit@gmail.com>
2024-11-11 20:01:43 +0900

    feat: add title and NavigateButton in Home screen

---
+++ src/screens/home/styled.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const HomeStyle = StyleSheet.create({
+  titleView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContianer: {
+    flex: 1,
+    alignItems: "center",
+    gap: 20,
+    paddingTop: 20,
+  },
+});
